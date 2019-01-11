3c80a7295692e240ac51d2e94d9b8f5a1f2305b6
Matthew Sevey <mjsevey@gmail.com>
2019-01-11 17:11:01 -0500

    metrics: create lifetime performance calculation and persist initial balance

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -6,11 +6,14 @@ package metrics
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 	"reflect"
 	"strconv"
 	"time"
 
 	"github.com/MSevey/traderbot/api"
+	"gitlab.com/NebulousLabs/Sia/persist"
 )
 
 // Metrics to Get and Track
@@ -34,28 +37,137 @@ import (
 //  5) Number of sells
 //      - Highest sell price
 //      - Lowest sell price
-//
-// TODO
-//
-// 1) persist data, start with writing json to disk
 
 type (
 	// Portfolio is the portfolio information pulled from the exchange
 	// (currently binance)
 	Portfolio struct {
-		Assets  []Asset
-		Updated time.Time
-		Value   float64
+		Assets  []Asset   `json:"assests"`
+		Updated time.Time `json:"updated"`
+		Value   float64   `json:"value"`
 	}
 
 	// Asset is the information about a coin held on an exchange (currently
 	// binance)
 	Asset struct {
-		Symbol   string
-		Quantity float64
-		Value    float64
+		Symbol   string  `json:"symbol"`
+		Quantity float64 `json:"quantity"`
+		Value    float64 `json:"value"`
+	}
+
+	// PortfolioPerformance contains information about the performance of the
+	// portfolio
+	PortfolioPerformance struct {
+		Assets []AssetPerformance
+	}
+
+	// AssetPerformance contains information about the performance of an asset
+	AssetPerformance struct {
+		Symbol               string
+		QtyIncreaseAbs       float64
+		QtyIncreasePercent   float64
+		ValueIncreaseAbs     float64
+		ValueIncreasePercent float64
 	}
 )
+
+var (
+	// metricsDir is the default directory where the trader metric data will be
+	// persisted
+	//
+	// NOTE: this assumes a linux system
+	metricsDir = filepath.Join(os.Getenv("HOME"), "tradermetrics")
+
+	// initialBalanceMetadata is the metadata for the persisted file that stores
+	// the initial starting balance for the trader
+	initialBalanceMetadata = persist.Metadata{
+		Header:  "InitialBalance",
+		Version: "v1.0.0",
+	}
+
+	// initialBalanceFile is the filename for the persisted file containing the
+	// initial balance of the trader
+	initialBalanceFile = "initialbalance"
+
+	// balanceExtension is the common file extension for the persisted balance
+	// files
+	balanceExtension = ".bal"
+)
+
+// LifeTimePortfolioPerformance calculates the lifetime performance of the
+// portfolio
+func LifeTimePortfolioPerformance() (PortfolioPerformance, error) {
+	// Get initial Balance
+	initial, err := initialBalance()
+	if err != nil {
+		return PortfolioPerformance{}, err
+	}
+	// Create map of assets for comparision
+	initialAssetMap := make(map[string]Asset)
+	for _, asset := range initial.Assets {
+		if _, ok := initialAssetMap[asset.Symbol]; ok {
+			continue
+		}
+		initialAssetMap[asset.Symbol] = asset
+	}
+
+	// Get Current balance
+	current, err := PortfolioBalance()
+	if err != nil {
+		return PortfolioPerformance{}, err
+	}
+
+	// Calculate performance
+	//
+	// NOTE: Ignore any assets in initial balance that aren't in current balance
+	var performance PortfolioPerformance
+	for _, asset := range current.Assets {
+		var ap AssetPerformance
+		// Check if asset was part of initial balance
+		initialAsset, ok := initialAssetMap[asset.Symbol]
+		if ok {
+			// Calculate performance
+			ap.Symbol = asset.Symbol
+			ap.QtyIncreaseAbs = asset.Quantity - initialAsset.Quantity
+			ap.QtyIncreasePercent = ((asset.Quantity - initialAsset.Quantity) / initialAsset.Quantity) * 100
+			ap.ValueIncreaseAbs = asset.Value - initialAsset.Value
+			ap.ValueIncreasePercent = ((asset.Value - initialAsset.Value) / initialAsset.Value) * 100
+			// Add to portfolio performance
+			performance.Assets = append(performance.Assets, ap)
+			continue
+		}
+		// Calculate performance
+		ap.Symbol = asset.Symbol
+		ap.QtyIncreaseAbs = asset.Quantity
+		ap.QtyIncreasePercent = 100
+		ap.ValueIncreaseAbs = asset.Value
+		ap.ValueIncreasePercent = 100
+		// Add to portfolio performance
+		performance.Assets = append(performance.Assets, ap)
+	}
+
+	return performance, nil
+}
+
+// initialBalance returns the initial balance of the trader that was saved on
+// disk. If there is not an initial balance found on disk one will be generated
+func initialBalance() (Portfolio, error) {
+	filename := filepath.Join(metricsDir, initialBalanceFile+balanceExtension)
+	portfolio := Portfolio{}
+	err := persist.LoadJSON(initialBalanceMetadata, portfolio, filename)
+	if os.IsNotExist(err) {
+		// No initial balance found, create an initial balance
+		if err = os.MkdirAll(metricsDir, 0777); err != nil {
+			return Portfolio{}, err
+		}
+		portfolio, err = PortfolioBalance()
+		err = persist.SaveJSON(initialBalanceMetadata, portfolio, filename)
+	}
+	if err != nil {
+		return Portfolio{}, err
+	}
+	return portfolio, nil
+}
 
 // PortfolioBalance returns the binance portfolio, listing the coins held and
 // the quantities and values of each
